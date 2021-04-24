08c08358c774d5df8331f0daf87cb63ce9e7769d
Manish Kumar <manish.kumar@autino.com>
2021-04-25 01:59:19 +0530

    routing and specific user page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 import Users from "./components/users/Users";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/users/User";
 import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -27,12 +33,30 @@ class App extends Component {
   // }
 
   searchUsers = async username => {
+    this.setState({
+      loading: true,
+    });
     console.log(username);
     const res = await axios.get(
       `https://api.github.com/search/users?q=${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({
       users: res.data.items,
+      loading: false,
+    });
+  };
+
+  getUser = async username => {
+    console.log(username);
+    this.setState({
+      loading: true,
+    });
+    console.log(username);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({
+      user: res.data,
       loading: false,
     });
   };
@@ -44,20 +68,62 @@ class App extends Component {
     });
   };
 
+  setAlert = (msg, type) => {
+    console.log(msg, type);
+    this.setState({
+      alert: {
+        msg: msg,
+        type: type,
+      },
+    });
+    setTimeout(() => {
+      this.setState({
+        alert: null,
+      });
+    }, 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { user, users, loading } = this.state;
     return (
-      <div className='App'>
-        <Navbar title='Github-Finder' icon='fab fa-github' />
-        <div className='container'>
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0}
-          />
-          <Users users={users} loading={loading} />
+      <Router>
+        <div className='App'>
+          <Navbar title='Github-Finder' icon='fab fa-github' />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0}
+                      setAlert={this.setAlert}
+                    />
+                    <Users users={users} loading={loading} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:username'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -20,10 +20,14 @@ class Search extends Component {
 
   searchSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({
-      text: "",
-    });
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({
+        text: "",
+      });
+    }
   };
 
   render() {
